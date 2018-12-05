56801e07762f9703605abae9a48e12c2bd957c16
jude90 <jude90@gitee.com>
2018-12-05 09:45:21 +0800

    first commit

---
+++ src/main/java/com/devops/echoservice/EchoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.devops.echoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/devops/echoservice/controller/EchoController.java
@@ -0,0 +1,17 @@
+package com.devops.echoservice.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jude on 2018/12/5.
+ */
+@RestController
+@RequestMapping("/v1")
+public class EchoController {
+    @RequestMapping("/echo/{words}")
+    public String greet(@PathVariable("words") String words){
+        return "Greeting !!! "+ words;
+    }
+}
